9d3399d9f505bb07b731c70650de353f86b33904
David G <85518858+dgalanberasaluce@users.noreply.github.com>
2022-12-11 14:46:59 +0100

    day05 - part1

---
+++ 2022/day5/main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import "testing"
+
+func TestDay5(t *testing.T) {
+
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
+	var lines = []string{
+		"    [D]    ",
+		"[N] [C]    ",
+		"[Z] [M] [P]",
+		" 1   2   3 ",
+		"",
+		"move 1 from 2 to 1",
+		"move 3 from 1 to 3",
+		"move 2 from 2 to 1",
+		"move 1 from 1 to 2",
+	}
+
+	t.Run("running part 1:", func(t *testing.T) {
+
+		got := Part1(lines)
+		want := "CMZ"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+}
