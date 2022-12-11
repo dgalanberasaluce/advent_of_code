91101388d878a5cba12cf6d761d13a17c06eab2a
David G <85518858+dgalanberasaluce@users.noreply.github.com>
2022-12-11 15:36:10 +0100

    remove duplication

--- 2022/day5/main.go
+++ 2022/day5/main.go
@@ -56,7 +56,7 @@ func reverse(s string) string {
 	return rev
 }
 
-func Part1(lines []string) string {
+func solve(lines []string, rev bool) string {
 	re1 := regexp.MustCompile(`^[ ]*(\[[A-Z]\][ ]*)+$`)
 	re2 := regexp.MustCompile(`^[ ]*([1-9][ ]*)+$`)
 	re3 := regexp.MustCompile(`^move ([1-9]+[0-9]*) from ([0-9]) to ([0-9])$`)
@@ -90,9 +90,12 @@ func Part1(lines []string) string {
 			var dst_stack = matchesRe3[0][3]
 
 			if len(cranes[stacks[src_stack]]) >= movements {
-				cranes[stacks[dst_stack]] = reverse(cranes[stacks[src_stack]][:movements]) + cranes[stacks[dst_stack]]
+				if rev {
+					cranes[stacks[dst_stack]] = reverse(cranes[stacks[src_stack]][:movements]) + cranes[stacks[dst_stack]]
+				} else {
+					cranes[stacks[dst_stack]] = cranes[stacks[src_stack]][:movements] + cranes[stacks[dst_stack]]
+				}
 				cranes[stacks[src_stack]] = cranes[stacks[src_stack]][movements:]
-
 			}
 		}
 	}
@@ -105,53 +108,12 @@ func Part1(lines []string) string {
 	return final_word
 }
 
+func Part1(lines []string) string {
+	return solve(lines, true)
+}
+
 func Part2(lines []string) string {
-	re1 := regexp.MustCompile(`^[ ]*(\[[A-Z]\][ ]*)+$`)
-	re2 := regexp.MustCompile(`^[ ]*([1-9][ ]*)+$`)
-	re3 := regexp.MustCompile(`^move ([1-9]+[0-9]*) from ([0-9]) to ([0-9])$`)
-
-	cranes := map[string]string{}
-	stacks := map[string]string{}
-
-	for _, line := range lines {
-		matchesRe1 := re1.FindAllStringSubmatch(line, -1)
-		if len(matchesRe1) > 0 {
-			for i, crate := range line {
-				if unicode.IsLetter(crate) {
-					cranes[strconv.Itoa(i)] += string(crate)
-				}
-			}
-		}
-
-		matchesRe2 := re2.FindAllStringSubmatch(line, -1)
-		if len(matchesRe2) > 0 {
-			for i, stack := range line {
-				if unicode.IsDigit(stack) {
-					stacks[string(stack)] = strconv.Itoa(i)
-				}
-			}
-		}
-
-		matchesRe3 := re3.FindAllStringSubmatch(line, -1)
-		if len(matchesRe3) > 0 {
-			movements, _ := strconv.Atoi(matchesRe3[0][1])
-			var src_stack = matchesRe3[0][2]
-			var dst_stack = matchesRe3[0][3]
-
-			if len(cranes[stacks[src_stack]]) >= movements {
-				cranes[stacks[dst_stack]] = cranes[stacks[src_stack]][:movements] + cranes[stacks[dst_stack]]
-				cranes[stacks[src_stack]] = cranes[stacks[src_stack]][movements:]
-			}
-		}
-	}
-
-	var final_word string
-	for i := 1; i <= len(stacks); i++ {
-		final_word += string(cranes[stacks[strconv.Itoa(i)]][0])
-	}
-
-	return final_word
-
+	return solve(lines, false)
 }
 
 func main() {
