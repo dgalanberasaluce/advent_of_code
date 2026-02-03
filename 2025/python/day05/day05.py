4fbe4fedb1355f93a2aaf1ce7fff25b0fb59b4cb
David G. <85518858+dgalanberasaluce@users.noreply.github.com>
2026-02-03 20:17:28 +0100

    aoc 2025 days 5.1 (optimized) python

--- 2025/python/day05/day05.py
+++ 2025/python/day05/day05.py
@@ -1,3 +1,6 @@
+import bisect
+import time
+
 def solve1(fresh_ingredients_range, avail):
     acc = 0
 
@@ -7,6 +10,38 @@ def solve1(fresh_ingredients_range, avail):
                 acc += 1
                 break
     return acc
+
+def solve1_opt(r_opt, avail):
+
+    start_points = [r[0] for r in r_opt]
+    fresh_count = 0
+
+    for val in avail:
+        idx = bisect.bisect_right(start_points, val) - 1
+
+        if idx >= 0:
+            range_start, range_end = r_opt[idx]
+            if range_start <= val <= range_end:
+                fresh_count += 1
+    
+    return fresh_count
+
+def get_optimized_range(fresh_ingredients_range):
+    sorted_range = sorted(fresh_ingredients_range, key=lambda x: x[0])
+    merged_ranges = []
+
+    curr_start, curr_end = sorted_range[0]
+    for i in range(1, len(sorted_range)):
+        next_start, next_end = sorted_range[i]
+
+        if next_start <= curr_end + 1:
+            curr_end = max(curr_end, next_end)
+        else:
+            merged_ranges.append((curr_start, curr_end))
+            curr_start, curr_end = next_start, next_end
+        merged_ranges.append((curr_start, curr_end))
+
+    return merged_ranges
 
 
 def pre(filename):
@@ -34,5 +69,22 @@ def part1():
     r, avail = pre("input.txt")
     print(f"Solution Test: {solve1(r, avail)}")
 
+def part1_t():
+    start_time = time.time()
+    r, avail = pre("input.txt")
+    sol = solve1(r,avail)
+    print(sol)
+    print(f"Time part1: {time.time() - start_time}")
+
+
+def part1_opt_t():
+    start_time = time.time()
+    r, avail = pre("input.txt")
+    r_opt = get_optimized_range(r)
+    sol = solve1_opt(r_opt, avail)
+    print(sol)
+    print(f"Time part1 optimized: {time.time() - start_time}")
+
 if __name__ == "__main__":
-    part1()
+    part1_t()
+    part1_opt_t()
