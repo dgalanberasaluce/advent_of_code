ff76dda46f5df2c483455bcdd8b3cd007278fe01
David G. <85518858+dgalanberasaluce@users.noreply.github.com>
2026-02-07 23:17:06 +0100

    aoc 2025 day08 part 2

--- 2025/python/day08/day08.py
+++ 2025/python/day08/day08.py
@@ -13,22 +13,50 @@ class Point:
     def __repr__(self):
         return f"Point(x={self.x}, y={self.y}, z={self.z})"
 
+class DSU():
+    def __init__(self, n):
+        self.parent = list(range(n))
+
+    def find(self, i):
+        if self.parent[i] != i:
+            self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+
+    def union(self, i, j):
+        root_i = self.find(i)
+        root_j = self.find(j)
+        if root_i != root_j:
+            self.parent[root_i] = root_j
+            return True
+        return False
+    
+    def get_all_sets(self):
+        from collections import defaultdict
+        sets = defaultdict(list)
+
+        for i in range(len(self.parent)):
+            root = self.find(i)
+            sets[root].append(i)
+        
+        return dict(sets)
+
 def squared_distance(p1:Point, p2:Point):
     return (p1.x - p2.x)**2 + (p1.y - p2.y)**2 + (p1.z - p2.z)**2
 
-def solve1_bruteforce(filename, threshold):
+def get_points(filename: str) -> list(Points):
     points = []
-    distances = {}
-
     with open(filename) as f:
         for line in f.readlines():
             point = line.strip().split(",")
             p = Point.from_list(point)
             points.append(p)
 
-    # --- Part 1: Calculate Distances ---
-    # Calculate the distance from P1 to P2 for all points
-    # We store: (squared_distance, idx1, idx2)
+    return points
+
+# --- Part 1: Calculate Distances ---
+# Calculate the distance from P1 to P2 for all points
+# We store: (squared_distance, idx1, idx2)
+def get_distances(points: list(Points)) -> list(tuple):
     pairs = []
     for i, p1 in enumerate(points):
         for j, p2 in enumerate(points):
@@ -39,32 +67,25 @@ def solve1_bruteforce(filename, threshold):
     # Sort by distance (smallest first)
     pairs.sort(key=lambda x: x[0])
 
+    return pairs
+
+def solve1_bruteforce(filename, threshold):
+    points = get_points(filename)
+    pairs = get_distances(points)
+
     # --- Part 2: Find the circuits of junction boxes from the THRESHOLD shortest distances ---
     # Union-Find Data Structure Initialization
-    parent = list(range(len(points)))
-
-    def find(i):
-        if parent[i] != i:
-            parent[i] = find(parent[i])
-        return parent[i]
-
-    def union(i, j):
-        root_i = find(i)
-        root_j = find(j)
-        if root_i != root_j:
-            parent[root_i] = root_j
-            return True
-        return False
+    dsu = DSU(len(points))
 
     for _, idx_a, idx_b in pairs[:threshold]:
-        union(idx_a, idx_b)
+        dsu.union(idx_a, idx_b)
     
     # --- Part 3: Retrieve the size of each circuit ---
     # circuit_sizes = { idx_root_a -> size, idx_root_b -> size }
     circuit_sizes = defaultdict(int)
 
     for i in range(len(points)):
-        root = find(i)
+        root = dsu.find(i)
         circuit_sizes[root] += 1
 
     sizes = sorted(circuit_sizes.values(), reverse=True)
@@ -91,48 +112,16 @@ def solve1_bruteforce(filename, threshold):
     return ans
 
 def solve2_bruteforce(filename):
-    points = []
-    distances = {}
-
-    with open(filename) as f:
-        for line in f.readlines():
-            point = line.strip().split(",")
-            p = Point.from_list(point)
-            points.append(p)
-
-    # --- Part 1: Calculate Distances ---
-    # Calculate the distance from P1 to P2 for all points
-    # We store: (squared_distance, idx1, idx2)
-    pairs = []
-    for i, p1 in enumerate(points):
-        for j, p2 in enumerate(points):
-            if i < j:
-                dist_sq = squared_distance(p1, p2)
-                pairs.append((dist_sq, i, j))
-
-    # Sort by distance (smallest first)
-    pairs.sort(key=lambda x: x[0])
+    points = get_points(filename)
+    pairs = get_distances(points)
 
     # --- Part 2: Find the circuits of junction boxes from the THRESHOLD shortest distances ---
     # Union-Find Data Structure Initialization
-    parent = list(range(len(points)))
-
-    def find(i):
-        if parent[i] != i:
-            parent[i] = find(parent[i])
-        return parent[i]
-
-    def union(i, j):
-        root_i = find(i)
-        root_j = find(j)
-        if root_i != root_j:
-            parent[root_i] = root_j
-            return True
-        return False
+    dsu = DSU(len(points))
 
     last_idx_a, last_idx_b = 0, 0
     for _, idx_a, idx_b in pairs:
-        if union(idx_a, idx_b):
+        if dsu.union(idx_a, idx_b):
             last_idx_a, last_idx_b = idx_a, idx_b
 
     return points[last_idx_a].x * points[last_idx_b].x
