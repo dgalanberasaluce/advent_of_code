d413eb1cdaba6dbd7bdea5b0d9b29820a332ead1
David G. <85518858+dgalanberasaluce@users.noreply.github.com>
2026-02-07 21:10:10 +0100

    aoc 2025 day08 part 2

--- 2025/python/day08/day08.py
+++ 2025/python/day08/day08.py
@@ -16,7 +16,6 @@ class Point:
 def squared_distance(p1:Point, p2:Point):
     return (p1.x - p2.x)**2 + (p1.y - p2.y)**2 + (p1.z - p2.z)**2
 
-
 def solve1_bruteforce(filename, threshold):
     points = []
     distances = {}
@@ -91,9 +90,58 @@ def solve1_bruteforce(filename, threshold):
     
     return ans
 
+def solve2_bruteforce(filename):
+    points = []
+    distances = {}
+
+    with open(filename) as f:
+        for line in f.readlines():
+            point = line.strip().split(",")
+            p = Point.from_list(point)
+            points.append(p)
+
+    # --- Part 1: Calculate Distances ---
+    # Calculate the distance from P1 to P2 for all points
+    # We store: (squared_distance, idx1, idx2)
+    pairs = []
+    for i, p1 in enumerate(points):
+        for j, p2 in enumerate(points):
+            if i < j:
+                dist_sq = squared_distance(p1, p2)
+                pairs.append((dist_sq, i, j))
+
+    # Sort by distance (smallest first)
+    pairs.sort(key=lambda x: x[0])
+
+    # --- Part 2: Find the circuits of junction boxes from the THRESHOLD shortest distances ---
+    # Union-Find Data Structure Initialization
+    parent = list(range(len(points)))
+
+    def find(i):
+        if parent[i] != i:
+            parent[i] = find(parent[i])
+        return parent[i]
+
+    def union(i, j):
+        root_i = find(i)
+        root_j = find(j)
+        if root_i != root_j:
+            parent[root_i] = root_j
+            return True
+        return False
+
+    last_idx_a, last_idx_b = 0, 0
+    for _, idx_a, idx_b in pairs:
+        if union(idx_a, idx_b):
+            last_idx_a, last_idx_b = idx_a, idx_b
+
+    return points[last_idx_a].x * points[last_idx_b].x
 
 def solve1(filename, threshold = 10):
     return solve1_bruteforce(filename, threshold)
+
+def solve2(filename):
+    return solve2_bruteforce(filename)
 
 def part1():
     expected = 40
@@ -102,6 +150,13 @@ def part1():
     
     print(f"Solution Test: {solve1("input.txt", 1000)}")
 
+def part2():
+    expected = 25272
+    sol = solve2("sample.txt")
+    print(f"{"PASS" if expected == sol else "FAILED"} solution: {sol} - expected: {expected}")
+    
+    print(f"Solution Test: {solve2("input.txt")}")
+
 if __name__ == "__main__":
     part1() # 153328
-    # part2()
+    part2() # 6095621910
