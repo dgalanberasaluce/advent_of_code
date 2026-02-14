8c3c543fbfa42df1eeb0388c618fe0e1b2a25911
David G. <85518858+dgalanberasaluce@users.noreply.github.com>
2026-02-14 12:48:44 +0100

    aoc 2024 day07 part 2 (dfs)

--- 2024/day07/day07.py
+++ 2024/day07/day07.py
@@ -1,8 +1,4 @@
-class NodeTree:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
+import math
 
 
 def parse_file(filename: str) -> list[tuple[int, list[int]]]:
@@ -29,6 +25,26 @@ def check_valid(nums, target):
     return dfs(1, nums[0])
 
 
+def check_valid_concat(nums, target):
+    def dfs(index, current_val):
+        if current_val > target:  # Optimization: Pruning
+            return False
+
+        if index == len(nums):
+            return current_val == target
+
+        # n_digits = len(str(nums[index]))
+        n_digits = int(math.log10(nums[index])) + 1
+
+        return (
+            dfs(index + 1, nums[index] + current_val)
+            or dfs(index + 1, nums[index] * current_val)
+            or dfs(index + 1, nums[index] + current_val * 10**n_digits)
+        )
+
+    return dfs(1, nums[0])
+
+
 def solve1(filename):
     parsed = parse_file(filename)
 
@@ -44,7 +60,15 @@ def solve1(filename):
 
 
 def solve2(filename):
-    pass
+    parsed = parse_file(filename)
+
+    ans = 0
+    for target, nums in parsed:
+        if check_valid_concat(nums, target):
+            ans += target
+        # else:
+        # print(nums, target)
+    return ans
 
 
 def part1():
@@ -58,7 +82,7 @@ def part1():
 
 
 def part2():
-    expected = -1
+    expected = 11387
     sol = solve2("sample.txt")
     print(
         f"{"PASS" if expected == sol else "FAILED"} solution: {sol} - expected: {expected}"
@@ -69,4 +93,4 @@ def part2():
 
 if __name__ == "__main__":
     part1()  # 2437272016585
-    # part2()  #
+    part2()  # 162987117690649
